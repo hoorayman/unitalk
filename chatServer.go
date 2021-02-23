3f31522e1eb436df92462200d0e9bef496191c7e
hoo <813458665@qq.com>
2021-02-23 23:56:03 +0800

    update

--- chatServer.go
+++ chatServer.go
@@ -1,10 +1,12 @@
 package main
 
+// WsServer define
 type WsServer struct {
 	clients    map[*Client]bool
 	register   chan *Client
 	unregister chan *Client
 	broadcast  chan []byte
+	rooms      map[*Room]bool
 }
 
 // NewWebsocketServer creates a new WsServer type
@@ -14,6 +16,7 @@ func NewWebsocketServer() *WsServer {
 		register:   make(chan *Client),
 		unregister: make(chan *Client),
 		broadcast:  make(chan []byte),
+		rooms:      make(map[*Room]bool),
 	}
 }
 
@@ -21,17 +24,13 @@ func NewWebsocketServer() *WsServer {
 func (server *WsServer) Run() {
 	for {
 		select {
-
 		case client := <-server.register:
 			server.registerClient(client)
-
 		case client := <-server.unregister:
 			server.unregisterClient(client)
-
 		case message := <-server.broadcast:
 			server.broadcastToClients(message)
 		}
-
 	}
 }
 
@@ -49,4 +48,22 @@ func (server *WsServer) broadcastToClients(message []byte) {
 	for client := range server.clients {
 		client.send <- message
 	}
+}
+
+func (server *WsServer) findRoomByName(name string) *Room {
+	var foundRoom *Room
+	for room := range server.rooms {
+		if room.GetName() == name {
+			foundRoom = room
+			break
+		}
+	}
+	return foundRoom
+}
+
+func (server *WsServer) createRoom(name string) *Room {
+	room := NewRoom(name)
+	go room.RunRoom()
+	server.rooms[room] = true
+	return room
 }
--- room.go
+++ room.go
@@ -1,56 +1,51 @@
 package main
 
-import "fmt"
-
+// Room define
 type Room struct {
-    name       string
-    clients    map[*Client]bool
-    register   chan *Client
-    unregister chan *Client
-    broadcast  chan *Message
+	name       string
+	clients    map[*Client]bool
+	register   chan *Client
+	unregister chan *Client
+	broadcast  chan *Message
 }
 
 // NewRoom creates a new Room
 func NewRoom(name string) *Room {
-    return &Room{
-        name:       name,
-        clients:    make(map[*Client]bool),
-        register:   make(chan *Client),
-        unregister: make(chan *Client),
-        broadcast:  make(chan *Message),
-    }
+	return &Room{
+		name:       name,
+		clients:    make(map[*Client]bool),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		broadcast:  make(chan *Message),
+	}
 }
 
 // RunRoom runs our room, accepting various requests
 func (room *Room) RunRoom() {
-    for {
-        select {
-
-        case client := <-room.register:
-            room.registerClientInRoom(client)
-
-        case client := <-room.unregister:
-            room.unregisterClientInRoom(client)
-
-        case message := <-room.broadcast:
-            room.broadcastToClientsInRoom(message)
-        }
-    }
+	for {
+		select {
+		case client := <-room.register:
+			room.registerClientInRoom(client)
+		case client := <-room.unregister:
+			room.unregisterClientInRoom(client)
+		case message := <-room.broadcast:
+			room.broadcastToClientsInRoom(message)
+		}
+	}
 }
 
 func (room *Room) registerClientInRoom(client *Client) {
-    room.notifyClientJoined(client)
-    room.clients[client] = true
+	room.clients[client] = true
 }
 
 func (room *Room) unregisterClientInRoom(client *Client) {
-    if _, ok := room.clients[client]; ok {
-        delete(room.clients, client)
-    }
+	if _, ok := room.clients[client]; ok {
+		delete(room.clients, client)
+	}
 }
 
 func (room *Room) broadcastToClientsInRoom(message []byte) {
-    for client := range room.clients {
-        client.send <- message
-    }
+	for client := range room.clients {
+		client.send <- message
+	}
 }
